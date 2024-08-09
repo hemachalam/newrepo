2b3d79f47f20e3b41a5dea0911a684178fc0ae16
hemachalam <hemachalamreddy@gmail.com>
2024-08-09 12:18:28 +0530

    case and operators bash file transfer to github

---
+++ case-statement.sh
@@ -0,0 +1,12 @@
+Name="Hemanth"
+case "$Name" in 
+	#case1
+	"peter") echo "Profession : Software Engineer" ;;
+
+	#case2 
+	"jambo") echo "Profession : Web Developer" ;;
+
+	#case3
+	"Hemanth") echo "Profession : Cloud and Devops Engineer" ;;
+esac
+	
---
+++ operators.sh
@@ -0,0 +1,11 @@
+if [ 10 -eq 10 ];
+then
+	echo "Equal"
+fi
+if
+	[ 'Hemanth' == 'Hemanth' ];
+then 
+	echo "same"   #output
+else
+	echo "not same"
+fi
